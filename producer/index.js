40c9aa894dbab9e2541abbf64b0993568f51142f
Kunal Bhat <kunal.bhat2001@gmail.com>
2022-03-18 14:08:22 +0530

    amqp client in producer

--- producer/index.js
+++ producer/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const amqp = require("amqplib/callback_api");
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
@@ -10,6 +11,23 @@ app.post("/new_ride", (req,res)=>{
     try{
         console.log(req.body)
         res.status(200).send("success");
+        amqp.connect('amqp://localhost', (err0, conn)=>{
+            if(err0) throw err0;
+            else {
+                conn.createChannel((err1,channel)=>{
+                    if(err1) throw err1;
+                    else {
+                        channel.assertQueue("test", {
+                            durable: false
+                        })
+                        channel.sendToQueue("test", Buffer.from("hello world"))
+                    }
+                })
+                setTimeout(()=>{
+                    conn.close()
+                }, 2000)
+            }
+        })
     } catch {
         res.status(400).send("Request missing details");
     }
